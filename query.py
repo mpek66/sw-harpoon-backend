e38270449d61da689b1f4a0e19a0e984f3cb69df
Mark Pekala <mpek66@gmail.com>
2018-07-21 13:33:44 -0500

    Bytes mode?

--- query.py
+++ query.py
@@ -8,8 +8,8 @@ def get(id, key):
     try:
         realpath = os.path.dirname(os.path.realpath(__file__))+"/static"
         idpath = realpath + "/articles/" + id
-        with open(idpath + "/" + key + ".txt", "r+") as fin:
-            return str(fin.read().encode('utf-8').strip())
+        with open(idpath + "/" + key + ".txt", "rb+") as fin:
+            return str(fin.read())
     except Exception as e:
         print("ERROR: querying article with id " + id + " for " + key, e, )
         return "ERROR: querying article with id " + id + " for " + key + ", " + str(e)
